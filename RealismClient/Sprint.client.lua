6b8945ccaa22d4f0fabf1a10ed91a7c775105695
Hedreon <39467046+Hedreon@users.noreply.github.com>
2022-04-12 02:02:49 +0100

    Reformat Script.

--- RealismClient/Sprint.client.lua
+++ RealismClient/Sprint.client.lua
@@ -1,23 +1,23 @@
-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Hedreon, 2022
 -- Realism Sprint
-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 local UserInputService = game:GetService("UserInputService")
 local TweenService     = game:GetService("TweenService")
 local Players          = game:GetService("Players")
 
-local TweenData        = TweenInfo.new(0.25, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut, 0, false, 0)
-local Player           = Players.LocalPlayer
-local Character        = Player.Character or Player.CharacterAdded:Wait()
-local Camera           = workspace.CurrentCamera
+local TweenInformation = TweenInfo.new(0.25, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut, 0, false, 0)
+local CurrentCamera    = workspace.CurrentCamera
+local LocalPlayer      = Players.LocalPlayer
+local Character        = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
 
 script.Parent = Character
 
 UserInputService.InputBegan:Connect(function(Input)
 	if Input.UserInputType == Enum.UserInputType.Keyboard then
 		if Input.KeyCode == Enum.KeyCode.LeftShift then
-			TweenService:Create(Camera, TweenData, {FieldOfView = 80}):Play()
+			TweenService:Create(CurrentCamera, TweenInformation, {FieldOfView = 80}):Play()
 			Character.Humanoid.WalkSpeed = 25
 		end
 	end
@@ -26,7 +26,7 @@ end)
 UserInputService.InputEnded:Connect(function(Input)
 	if Input.UserInputType == Enum.UserInputType.Keyboard then
 		if Input.KeyCode == Enum.KeyCode.LeftShift then
-			TweenService:Create(Camera, TweenData, {FieldOfView = 70}):Play()
+			TweenService:Create(CurrentCamera, TweenInformation, {FieldOfView = 70}):Play()
 			Character.Humanoid.WalkSpeed = 16
 		end
 	end
